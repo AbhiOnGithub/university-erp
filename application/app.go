b9894898931356f8ce23196f298d47c0c75bd8e8
Abhishek Kumar Gupta <Abhishek.Kumar.Gupta1@citrix.com>
2021-05-06 22:17:06 +0530

    first commit

---
+++ application/app.go
@@ -0,0 +1,9 @@
+package application
+
+import (
+	"fmt"
+)
+
+func SetupApp() {
+	fmt.Println("Setting up application")
+}
---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/abhiongithub/university-erp/application/app"
+)
+
+func main() {
+	fmt.Println("Welcome to University ERP")
+
+	app.SetupApp()
+}
